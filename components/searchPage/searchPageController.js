b68925a4c822d033d187fb0e1c281ac43ed811f4
Jackson Lucas <jackson7br@gmail.com>
2016-08-31 12:57:44 -0400

    feat (searchPage) : controller receive results

--- components/searchPage/searchPageController.js
+++ components/searchPage/searchPageController.js
@@ -1,8 +1,9 @@
 (function () {
 
-function searchPageController ($scope, $location) {
+function searchPageController ($scope, $location, results) {
   console.log('Search Page')
   $scope.queryParameters = $location.search()
+  console.log(results)
 }
 
 angular.module("pixewsWeb").controller('searchPageController', searchPageController)
