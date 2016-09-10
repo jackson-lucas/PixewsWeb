720a5422e2133b7fbd072a963d29b0438499eb65
Jackson Lucas <jackson7br@gmail.com>
2016-09-10 14:32:34 -0400

    fix (home) : background

--- components/homePage/homePageController.js
+++ components/homePage/homePageController.js
@@ -1,6 +1,22 @@
 (function () {
 
-function homePageController ($scope, $window) {
+function homePageController ($scope, $location) {
+  angular.element(document).ready(function () {
+    $('.jumbotron').height($('body').height())
+    $('.jumbotron').css('margin-bottom', '0px')
+  })
+
+  $scope.search = function (tags) {
+    tags = tags.split(' ').reduce(function (previous, current) {
+      return previous + '+' + current
+    })
+
+    console.log('searching')
+    $location.path('/pesquisar').search({
+      'tags': tags
+    });
+    // .replace('\s', '+')
+  }
 }
 
 angular.module("pixewsWeb").controller('homePageController', homePageController)
