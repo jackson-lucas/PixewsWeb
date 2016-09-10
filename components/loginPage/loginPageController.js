e37ca9e8b0ed784356bd2aacbd1d16236748d87e
Your Name <you@example.com>
2016-09-10 17:46:50 -0400

    feat : form validations

--- components/loginPage/loginPageController.js
+++ components/loginPage/loginPageController.js
@@ -2,20 +2,62 @@
 
 function loginPageController ($scope, $window, apiService) {
 
+function isLoginValid () {
+    if($scope.email == null || $scope.email == "" || $scope.senha == null || $scope.senha == ""){
+      //alert("Campo vazio");
+      $.notify({
+        title: '<strong>Aviso!</strong>',
+        message: 'Campo vazio.'
+      },{
+        type: 'danger'
+      });
+      return false;
+    }
+
+    var x = $scope.email;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        //alert("E-mail não válido");
+        $.notify({
+          title: '<strong>Aviso!</strong>',
+          message: 'E-mail inválido.'
+        },{
+        type: 'danger'
+      });
+        return false;
+    }
+
+    var illegalChars = /[\W_]/; // allow only letters and numbers
+    if ($scope.senha.length < 4 || $scope.senha.length > 16 || illegalChars.test($scope.senha)) {
+          
+        //alert("Senha não aceita! Mínimo de 4 caracteres");
+        $.notify({
+          title: '<strong>Aviso!Senha inválida</strong>',
+          message: 'Mínimo de 4 caracteres.'
+        },{
+        type: 'danger'
+      });
+        return false;
+      }
+
+}
+
   $scope.login = function (email, senha) {
+    if (isLoginValid()) {
     apiService.login(email, senha)
       .then(function success (response) {
-        console.log('logged')
-        console.log(response)
-        $window.localStorage.token = response.data.token
+        console.log('logged');
+        console.log(response);
+        $window.localStorage.token = response.data.token;
         // $window.localStorage.usuario = response.data.usuario.id
-        $window.localStorage.chave = response.data.chave
+        $window.localStorage.chave = response.data.chave;
       }, function error (response) {
-        console.error(response)
+        console.error(response);
       })
   }
 }
-
+}
 angular.module("pixewsWeb").controller('loginPageController', loginPageController)
 
 })()
--- components/registerPage/registerPageController.js
+++ components/registerPage/registerPageController.js
@@ -3,6 +3,71 @@
 function registerPageController ($scope, apiService, $location) {
 
   function isRegistrationValid () {
+    if($scope.name == null || $scope.name == ""
+      || $scope.email == null || $scope.email == ""
+      || $scope.cnpj == null || $scope.cnpj == ""
+      || $scope.password == null || $scope.password == ""
+      || $scope.passwordConfirmation == null || $scope.passwordConfirmation == ""){
+      //alert("Campo vazio");
+      $.notify({
+        title: '<strong>Aviso!</strong>',
+        message: 'Campo vazio.'
+      },{
+        type: 'danger'
+      });
+      return false;
+    }
+
+    var x = $scope.email;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        //alert("E-mail não válido");
+        $.notify({
+          title: '<strong>Aviso!</strong>',
+          message: 'E-mail inválido.'
+        },{
+        type: 'danger'
+      });
+        return false;
+    }
+
+    var illegalChars = /[\W_]/; // allow only letters and numbers
+    if ($scope.password.length < 4 || $scope.password.length > 16 || illegalChars.test($scope.password)) {
+          
+        //alert("Senha não aceita! Mínimo de 4 caracteres");
+        $.notify({
+          title: '<strong>Aviso!Senha inválida</strong>',
+          message: 'Mínimo de 4 caracteres.'
+        },{
+        type: 'danger'
+      });
+        return false;
+      }
+
+      if ($scope.passwordConfirmation.length < 4 || $scope.password.length > 16 || illegalChars.test($scope.password)) {
+        //alert("Senha não aceita! Mínimo de 4 caracteres");
+        $.notify({
+          title: '<strong>Aviso!Senha inválida</strong>',
+          message: 'Caracteres inválidos ou insuficientes.'
+        },{
+        type: 'danger'
+      });
+        return false;
+      }
+
+      var illegalPass = /[0-9]+/; // allow only numbers
+      if ($scope.cnpj.length < 14 || !illegalPass.test($scope.cnpj)) {
+        //alert("CNPJ inválido!");
+        $.notify({
+        title: '<strong>Aviso!</strong>',
+        message: 'CNPJ inválido.'
+      },{
+        type: 'danger'
+      });
+        return false;
+      }
+
     if ($scope.password != $scope.passwordConfirmation)
       return false
     return true
