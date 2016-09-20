33acaa02d15f5ce93a73e004132080cb7aa003a5
Jackson Lucas <jackson7br@gmail.com>
2016-09-20 08:12:47 -0400

    fix : mosaic images

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ angular.module('pixewsWeb', [
 ]);
 
 angular.module('pixewsWeb').constant('config', {
-  baseApi: 'http://ec2-54-208-73-59.compute-1.amazonaws.com',
+  baseApi: 'http://ec2-52-23-224-27.compute-1.amazonaws.com',
   // baseApi: 'http://localhost:3000',
   publicToken: "2d7fEicnEirsPE"
 })
